b0afc093114d5c96c9818ba27fbdd972009d58c7
Oussama <oussrock@gmail.com>
2016-08-30 17:12:32 -0400

    update npm

--- app/app.js
+++ app/app.js
@@ -39,21 +39,15 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    res.send("Error 500");
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.send("Error 500");
 });
 
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var path = require('path');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Hello world");
+	var view_dir = path.join(__dirname, '../public/html');
+	var index_path = path.join(view_dir, "index.html");
+	res.sendFile(index_path);
 });
 
 module.exports = router;
